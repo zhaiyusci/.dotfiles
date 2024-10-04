10a7f6166dce376d9fd909606e4fc90f16fa2cc5
Yu Zhai <yuzhai@pku.edu.cn>
2024-10-04 16:29:16 +0800

    leader

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,8 +36,8 @@ opt.mouse = ""
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- vim.g.mapleader = "\\"
+-- vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
